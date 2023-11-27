4846ca0a183e1e117c57990957afabec285125e8
Prempal Shekhawat <77876294+prempalshekhawat@users.noreply.github.com>
2023-11-27 18:12:15 +0530

    implemented room db

--- app/src/main/java/com/prempal/teachmintassignment/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/prempal/teachmintassignment/ui/screens/home/HomeScreen.kt
@@ -1,10 +1,12 @@
 package com.prempal.teachmintassignment.ui.screens.home
 
+import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -18,22 +20,29 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.navigation.NavController
+import androidx.navigation.NavHostController
 import androidx.paging.LoadState
 import androidx.paging.compose.collectAsLazyPagingItems
-import androidx.paging.compose.items
+import androidx.paging.compose.itemContentType
+import androidx.paging.compose.itemKey
 import com.prempal.teachmintassignment.ui.screens.MainViewModel
-import com.prempal.teachmintassignment.ui.screens.views.HomeCard
+import com.prempal.teachmintassignment.utils.NetworkUtils
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Home(navController: NavController, viewModel: MainViewModel = hiltViewModel()) {
-
+fun Home(navHostController: NavHostController, viewModel: MainViewModel = hiltViewModel()) {
     var query by remember { mutableStateOf("") }
+    var searchQuery by remember { mutableStateOf("") }
+    val lazyColumnListState = rememberLazyListState()
 
-    val dataset = viewModel.items().collectAsLazyPagingItems()
+    if (query == ""){
+        searchQuery = query
+    }
+
+    val dataset = viewModel.items(searchQuery).collectAsLazyPagingItems()
 
     Column {
         TextField(
@@ -47,7 +56,8 @@ fun Home(navController: NavController, viewModel: MainViewModel = hiltViewModel(
 
         Button(
             onClick = {
-                viewModel.updateQuery(query = query)
+                searchQuery = query
+//                viewModel.updateQuery(query = query)
             },
             modifier = Modifier
                 .fillMaxWidth()
@@ -56,62 +66,44 @@ fun Home(navController: NavController, viewModel: MainViewModel = hiltViewModel(
             Text("Search Repositories")
         }
 
-        LazyColumn {
-            items(dataset) {
-                HomeCard(it)
-            }
-
-            when (dataset.loadState.refresh) { //FIRST LOAD
-                is LoadState.Error -> {
-                    //TODO Error Item
-                    //state.error to get error message
-                }
-
-                is LoadState.Loading -> { // Loading UI
-                    item {
-                        Column(
-                            modifier = Modifier
-                                .fillParentMaxSize(),
-                            horizontalAlignment = Alignment.CenterHorizontally,
-                            verticalArrangement = Arrangement.Center,
-                        ) {
-                            Text(
-                                modifier = Modifier
-                                    .padding(8.dp),
-                                text = "Refresh Loading"
-                            )
-
-                            CircularProgressIndicator(color = Color.Black)
-                        }
-                    }
-                }
-
-                else -> {}
-            }
-
-            when (dataset.loadState.append) { // Pagination
-                is LoadState.Error -> {
-                    //TODO Pagination Error Item
-                    //state.error to get error message
-                }
-
-                is LoadState.Loading -> { // Pagination Loading UI
-                    item {
-                        Column(
-                            modifier = Modifier
-                                .fillMaxWidth(),
-                            horizontalAlignment = Alignment.CenterHorizontally,
-                            verticalArrangement = Arrangement.Center,
-                        ) {
-                            Text(text = "Pagination Loading")
-
-                            CircularProgressIndicator(color = Color.Black)
-                        }
-                    }
-                }
-
-                else -> {}
-            }
+    LazyColumn(state = lazyColumnListState) {
+        items(
+        count = dataset.itemCount,
+        key = dataset.itemKey(),
+        contentType = dataset.itemContentType(
+            )
+    ) { index ->
+        val item = dataset[index]
+        HomeCard(item, navHostController)
         }
+
+        when (dataset.loadState.refresh) {
+            is LoadState.Error -> {
+                //ToDo: to be handled
+            }
+
+            is LoadState.Loading -> { // Loading UI
+                item {
+                    Column(
+                        modifier = Modifier
+                            .fillParentMaxSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center,
+                    ) {
+                        Text(
+                            modifier = Modifier
+                                .padding(8.dp),
+                            text = "Loading"
+                        )
+
+                        CircularProgressIndicator(color = Color.Blue)
+                    }
+                }
+            }
+
+            else -> {}
+        }
+
+    }
     }
 }
